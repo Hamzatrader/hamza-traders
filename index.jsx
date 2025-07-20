fce27ee5f0711e61daefa0c6a1aa91378d98deaa
Hamzatrader <hamzaabbas51234890@gmail.com>
2025-07-20 06:41:45 +0500

    Create index.jsx

---
+++ index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import HamzaTraders from "./app";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-4">
+      <HamzaTraders />
+    </main>
+  );
+}
